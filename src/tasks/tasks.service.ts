5e3a9477a88ae061bb6941927777a651177efebc
nabechin <nabenabe0258@gmail.com>
2021-05-29 16:43:06 +0900

    create task modal

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = ['aaa'];
+  private tasks: Task[] = [];
 
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasks;
   }
 }
