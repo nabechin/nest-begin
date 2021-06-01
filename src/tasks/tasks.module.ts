1c84b317639d0d711ef5bb4484d489b08ea8556c
nabechin <nabenabe0258@gmail.com>
2021-06-01 22:21:19 +0900

    define repository

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,22 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TasksRepository } from './tasks.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'task-management',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([TasksRepository])],
   controllers: [TasksController],
   providers: [TasksService],
 })
