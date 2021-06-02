b3fc76c1a7433cc9af5121c82f6f2101c65733df
nabechin <nabenabe0258@gmail.com>
2021-06-02 22:55:20 +0900

    create auth module

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,12 @@
+import { PrimaryGeneratedColumn, Column } from 'typeorm';
+
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+}
