33829341b9d6f2b64973dfce4159019c5bb632fe
nabechin <nabenabe0258@gmail.com>
2021-06-03 21:33:29 +0900

    validate credentials

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, MinLength, MaxLength, Min, Matches } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password id too weak',
+  })
   password: string;
 }
